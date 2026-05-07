fee08e21fbb859b7cd9a7d654060c8b12acd3b78
Artem Shutov <artfergames@gmail.com>
2026-05-07 23:08:57 +0700

    feat: отображение времени суток + менюшка скрывается и раскрывается

--- src/components/UI/InfoPanel/InfoPanel.tsx
+++ src/components/UI/InfoPanel/InfoPanel.tsx
@@ -1,5 +1,6 @@
 import styles from "./InfoPanel.module.css";
 import { useGameStore } from "../../../Store/GameStore.ts";
+import { useRef, useState } from "react";
 
 function Row({ name, data }: { name: string; data: string | number }) {
   return (
@@ -11,14 +12,44 @@ function Row({ name, data }: { name: string; data: string | number }) {
 }
 
 export default function InfoPanel() {
+  const [isOpen, setIsOpen] = useState(true);
+  const contentRef = useRef<HTMLDivElement>(null);
+
   return (
     <div className={styles.infoPanel}>
-      <div className={styles.infoPanel__header}>
-        <div className={styles.infoPanel__title}>Весёлая ферма</div>
-        <div className={styles.infoPanel__chev}>&gt;</div>
+      <div
+        className={styles.infoPanel__header}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <div className={styles.infoPanel__title}>
+          <p>Весёлая ферма</p>
+        </div>
+        <div
+          className={styles.infoPanel__chev}
+          style={{
+            transform: isOpen ? "rotate(90deg)" : "rotate(0deg)",
+            transition: "0.3s",
+          }}
+        >
+          &gt;
+        </div>
       </div>
-      <div className={styles.infoPanel__body}>
+      <div
+        className={`${styles.infoPanel__body} ${isOpen ? "" : styles.infoPanel__body__close}`}
+        ref={contentRef}
+        style={{
+          height: isOpen ? `${contentRef.current?.scrollHeight}px` : "0px",
+        }}
+      >
         <div className={styles.infoPanel__divider}></div>
+        <Row
+          name={"Время суток"}
+          data={useGameStore((state) =>
+            state.gameState.meta.isNight ? "Ночь" : "День",
+          )}
+        />
         <Row
           name={"Баланс"}
           data={useGameStore((state) => state.gameState.economy.money)}
